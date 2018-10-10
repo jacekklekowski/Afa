1b0bbd78395d1e58cba933ccebc439c79d8e56c7
Jacek <jacekklekowski@wp.pl>
2018-10-10 17:36:33 +0200

    initial commint

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String [] args)
+    { byte age = 39;
+            String city = "Gdansk";
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
+
+
